a714532e1e1fc015e0269db63f13cd4c7a0d00ca
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-10-21 14:12:35 +0300

    Adding js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-function watchDB(){
-    fetch(' http://localhost:3000/films')
-    .then(res => res.json())
-    .then(data => console.log(data))
+// Define the API endpoint for movie data
+const apiEndpoint = 'http://localhost:3000/films';
+
+// Function to fetch movie data
+async function fetchMovieData() {
+    try {
+        const response = await fetch(apiEndpoint);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching movie data:', error);
+        return [];
+    }
 }
 
-watchDB();
+// Function to display movie details and film list
+function displayMovieDetailsAndList(movieData) {
+    const filmsList = document.getElementById('films');
+    const movieDetails = document.getElementById('movie-details');
+
+    // Populate the film list
+    filmsList.innerHTML = ''
+    movieData.forEach((movie) => {
+        const filmItem = document.createElement('li');
+        filmItem.classList.add('film', 'item');
+        filmItem.textContent = movie.title;
+
+        // Add a click event listener to display movie details when clicked
+        filmItem.addEventListener('click', () => {
+            displayMovieDetails(movie);
+        })
+
+        filmsList.appendChild(filmItem);
+    });
+
+    // Display details of the first movie by default
+    if (movieData.length > 0) {
+        displayMovieDetails(movieData[0]);
+    }
+}
